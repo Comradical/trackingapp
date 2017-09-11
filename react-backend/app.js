1fa5487718df606da37e4db0e67ce8ebfb96fdab
Brian Austgen <linguistic151@gmail.com>
2017-09-11 23:43:23 +0000

    2nd commit

--- react-backend/app.js
+++ react-backend/app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    passport   = require("passport"),
+    LocalStrategy = require("passport-local"),
+    mongoose   = require("mongoose"),
+    User = require("./models/user");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+
+var url = process.env.DATABASEURL || "mongodb://localhost/trackingapp";
+//setup the database we are using
+mongoose.connect(url);
 
 var app = express();
 
@@ -24,6 +32,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+//Authentication
+app.use(require("express-session")({
+    secret: "Once again Rusy wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//End authentication
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
