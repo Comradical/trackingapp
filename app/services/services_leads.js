c506c9efd7391586120a3ce4dfe1466597c02cbd
Brian Austgen <linguistic151@gmail.com>
2018-09-03 17:02:50 +0000

    refactor lead service

--- app/services/services_leads.js
+++ app/services/services_leads.js
@@ -1,33 +1,37 @@
-var Lead = require('../config/models/lead');
-var Campaign = require('../config/models/campaign');
-var leadServices = {};
+import Lead from '../config/models/lead'
+import Campaign from '../config/models/campaign'
+var leadServices = {}
+let error = {
+  data: null,
+  message: 'There was an error'
+}
 
+leadServices.createLead = function (lead, callback) {
+  var leadToCreate = {
+    firstname: lead.firstname,
+    lastname: lead.lastname,
+    email: lead.email,
+    phone: lead.phone,
+    campaigns: [lead.campaign],
+    events: [lead.event]
+  }
 
-leadServices.createLead = function(lead, callback){
-    var leadToCreate = {
-        firstname: lead.firstname,
-        lastname: lead.lastname,
-        email: lead.email,
-        phone: lead.phone,
-        campaigns: [lead.campaign],
-        events: [lead.event]
-    };
-    
-    Campaign.findById(lead.campaign, function(err, campaign){
-       if(err){
-           callback("Wow, we couldn't find your account.. This is awkward", null);
+  Campaign.findById(lead.campaign, function (err, campaign) {
+    if (err) {
+      error.data = err
+      callback(error, null)
+    } else {
+      Lead.create(leadToCreate, function (err, createdLead) {
+        if (err) {
+          callback(err, null)
         } else {
-            Lead.create(leadToCreate, function(err, createdLead){
-                if(err){
-                    callback(err, null);
-                } else {
-                    campaign.leads.push(createdLead);
-                    campaign.save();
-                    callback(null, createdLead);
-                }
-            });
+          campaign.leads.push(createdLead)
+          campaign.save()
+          callback(null, createdLead)
         }
-    });
-};
+      })
+    }
+  })
+}
 
-module.exports = leadServices;
+module.exports = leadServices
