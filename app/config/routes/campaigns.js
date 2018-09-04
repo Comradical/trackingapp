733cac1ad06486b00cbfbae01478bb31477c5197
Brian Austgen <linguistic151@gmail.com>
2018-09-04 16:27:19 +0000

    refactor campaigns

--- app/config/routes/campaigns.js
+++ app/config/routes/campaigns.js
@@ -1,56 +1,52 @@
-var cors = require('cors');
-var express = require('express');
-var router = express.Router();
-var campaignServices = require('../../services/services_campaigns');
-var errorHandler = require('../../services/errors');
-var app = express();
+import cors from 'cors'
+import express from 'express'
+import campaignServices from '../../services/services_campaigns'
+import errorHandler from '../../services/errors'
+
+// configure variables
+const router = express.Router()
+const app = express()
 
 // Hardcoded details
-var currentUser = require("../currentUser");
-
-//Allow cross domain acceess:
-//  /campaigns routes
+import currentUser from '../currentUser'
 
 // Get Campaigns
 router.get('/', cors(), (req, res, next) => {
-    campaignServices.fetchCampaigns(currentUser)
-    .then((response) => {
-        res.json(response);
-    })
-    .catch(errorHandler);
-});
+  campaignServices.fetchCampaigns(currentUser)
+  .then((response) => {
+    res.json(response)
+  })
+  .catch(errorHandler)
+})
 
 //Create Campaign
-router.post('/', function(req, res){
-    campaignServices.create(req.body, currentUser, function(err, success){
-        if(err){
-            errorHandler(err);
-        } else {
-            res.send(success);
-            console.log(success);
-        }
-    });
-});
+router.post('/', function(req, res) {
+  campaignServices.create(req.body, currentUser, function(err, success) {
+    if (err) {
+      errorHandler(err)
+    } else {
+      res.send(success)
+      console.log(success)
+    }
+  })
+})
 
-router.get('/:id', cors(), function(req, res){
-   campaignServices.fetchCampaign(req.params.id)
-    .then((response) => {
-        res.json(response);
-    })
-    .catch(errorHandler);
-});
+router.get('/:id', cors(), function(req, res) {
+ campaignServices.fetchCampaign(req.params.id)
+  .then((response) => {
+    res.json(response)
+  })
+  .catch(errorHandler)
+})
 
-router.put('/:id', function(req, res){
-    //TODO Update Campaign
-    res.send("Sorry, no way to update a campaign has been implemented yet");
-});
+router.put('/:id', function(req, res) {
+  //TODO Update Campaign
+  res.send("Sorry, no way to update a campaign has been implemented yet")
+})
 
-router.delete('/:id', function(req, res){
-    //TODO Delete a campaign
-   res.send("Sorry, a way to delete campaigns hsa not been implemented yet"); 
-});
+router.delete('/:id', function(req, res) {
+  //TODO Delete a campaign
+ res.send("Sorry, a way to delete campaigns hsa not been implemented yet") 
+})
 
-
-
-
-module.exports = router;
+module.exports = router
