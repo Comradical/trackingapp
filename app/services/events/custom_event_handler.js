62bb15928693a12fa5d2ad144c9d05ef87150b32
Brian Austgen <linguistic151@gmail.com>
2018-09-05 15:49:16 +0000

    refactor custom event handler

--- app/services/events/custom_event_handler.js
+++ app/services/events/custom_event_handler.js
@@ -1,21 +1,21 @@
-var customEvent = {};
-var Map = require('../../config/models/event_map');
+import Map from '../../config/models/event_map'
+let customEvent = {}
 
-customEvent.map = function (event, mappingId){
-    return new Promise((resolve, reject) => {
-        let eventToCreate = {};
-        Map.findById(mappingId, (err, foundMap) => {
-            let map = foundMap.mapping;
-            if(err){
-                reject(err);
-            } else {
-                for(var key in map){
-                    eventToCreate[key] = event[map[key]];
-                }
-            resolve(eventToCreate);
-            }
-        });
-    });
-};
+customEvent.map = (event, mappingId) => {
+  return new Promise((resolve, reject) => {
+    let eventToCreate = {}
+    Map.findById(mappingId, (err, foundMap) => {
+      let map = foundMap.mapping
+      if (err) {
+        reject(err)
+      } else {
+        for (var key in map) {
+          eventToCreate[key] = event[map[key]]
+        }
+        resolve(eventToCreate)
+      }
+    })
+  })
+}
 
-module.exports = customEvent;
+module.exports = customEvent
